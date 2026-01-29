0eabfd2e57daa95c2022bae5a5a3a980b7f14d5e
matthew <matthewapodaca24@gmail.com>
2026-01-29 15:48:15 -0800

    commit

---
+++ routes/employees.js
@@ -0,0 +1,43 @@
+import express from "express";
+import employees from "../db/employees.js";
+
+const router = express.Router();
+export default router;
+
+router.get("/", (req, res) => {
+  res.send(employees);
+});
+
+router.get("/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * employees.length);
+  res.send(employees[randomIndex]);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const employee = employees.find((e) => e.id === Number(id));
+
+  if (!employee) {
+    return res.status(404).send("Employee not found");
+  }
+
+  res.send(employee);
+});
+
+router.post("/", (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).send("Request must have a name");
+  }
+
+  const newEmployee = {
+    id: employees.length + 1,
+    name,
+  };
+
+  employees.push(newEmployee);
+
+  res.status(201).send(newEmployee);
+});
